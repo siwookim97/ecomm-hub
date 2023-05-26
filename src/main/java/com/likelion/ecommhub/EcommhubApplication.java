fe909a0a36e33880ff141a9e479102eba79ef80a
siwookim97 <tldn12gh@gmail.com>
2023-05-26 18:17:24 +0900

    init setting

---
+++ src/main/java/com/likelion/ecommhub/EcommhubApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.ecommhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/ecommhub/EcommhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.ecommhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
