8fc8f7925c28e24fa7b17c6b0eae49e8c35a0f04
HamSeungChan <head0618@naver.com>
2023-05-30 14:21:16 +0900

    test: Product 엔티티 데이터 저장 테스트 추가

---
+++ src/test/java/com/likelion/ecommhub/repository/ProductRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.likelion.ecommhub.repository;
+
+import com.likelion.ecommhub.domain.Product;
+import com.likelion.ecommhub.domain.ProductState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void init() {
+        Product productA = new Product(1L, "apple", "this is apple", ProductState.ON_SALE, 100);
+        Product productB = new Product(2L, "banana", "this is banana", ProductState.ON_SALE, 50);
+        Product productC = new Product(3L, "coke", "this is coke", ProductState.ON_SALE, 300);
+
+        productRepository.save(productA);
+        productRepository.save(productB);
+        productRepository.save(productC);
+    }
+
+    @Test
+    @DisplayName("Member 엔티티 save 확인 테스트")
+    void productInsertTest() {
+        int count_product = productRepository.findAll().size();
+
+        assertThat(count_product).isEqualTo(3);
+    }
+}
