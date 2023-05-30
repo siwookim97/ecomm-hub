cff24d3999447447d22431a5f72c5cf2d8ae25ed
siwookim97 <tldn12gh@gmail.com>
2023-05-30 14:43:22 +0900

    test: MemberService 테스트 추가

--- src/test/java/com/likelion/ecommhub/repository/MemberRepositoryTest.java
+++ src/test/java/com/likelion/ecommhub/repository/MemberRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.likelion.ecommhub.repository;
 
-import com.likelion.ecommhub.domain.Gender;
 import com.likelion.ecommhub.domain.Member;
 import com.likelion.ecommhub.domain.MemberRole;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,23 +22,23 @@ class MemberRepositoryTest {
     void init() {
         Member buyer_A = new Member("buyer_A", "buyer1234", "구매자1",
                 "buyer_A@gmail.com", "010-1111-1111", "서울시 노원구",
-                LocalDate.now(), Gender.MALE, MemberRole.ROLE_BUYER);
+                 MemberRole.ROLE_BUYER);
         Member buyer_B = new Member("buyer_B", "buyer1234", "구매자2",
                 "buyer_B@hanmail.net", "010-2222-2222", "창원시 귀현동",
-                LocalDate.now(), Gender.MALE, MemberRole.ROLE_BUYER);
+                MemberRole.ROLE_BUYER);
         Member buyer_C = new Member("buyer_A", "buyer1234", "구매자3",
                 "buyer_C@naver.com", "010-3333-3333", "서울시 반포동",
-                LocalDate.now(), Gender.FEMALE, MemberRole.ROLE_BUYER);
+                MemberRole.ROLE_BUYER);
         memberRepository.save(buyer_A);
         memberRepository.save(buyer_B);
         memberRepository.save(buyer_C);
 
         Member seller_A = new Member("seller_A", "seller1234", "판매자1",
                 "seller_A@naver.com", "010-4444-4444", "서울시 서초구",
-                LocalDate.now(), Gender.FEMALE, MemberRole.ROLE_SELLER);
+                MemberRole.ROLE_SELLER, "343-11111-111111");
         Member seller_B = new Member("seller_B", "seller1234", "판매자2",
                 "seller_B@gmail.com", "010-4444-4444", "서울시 강남구",
-                LocalDate.now(), Gender.MALE, MemberRole.ROLE_SELLER);
+                MemberRole.ROLE_SELLER, "434-33333-333333");
         memberRepository.save(seller_A);
         memberRepository.save(seller_B);
     }
---
+++ src/test/java/com/likelion/ecommhub/service/MemberServiceTest.java
@@ -0,0 +1,80 @@
+package com.likelion.ecommhub.service;
+
+import com.likelion.ecommhub.domain.Member;
+import com.likelion.ecommhub.domain.MemberRole;
+import com.likelion.ecommhub.dto.MemberJoinRequest;
+import com.likelion.ecommhub.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        Member sellerMember = new Member(
+                "seller",
+                "seller_password",
+                "seller_name",
+                "email1@google.com",
+                "010-1111-1111",
+                "seller_address",
+                MemberRole.ROLE_SELLER,
+                "account"
+        );
+        Member buyerMember = new Member(
+                "buyer",
+                "buyer_password",
+                "buyer_name",
+                "email2@google.com",
+                "010-2222-2222",
+                "buyer_address",
+                MemberRole.ROLE_SELLER
+        );
+        memberService.joinSeller(new MemberJoinRequest("seller", "seller_password", "seller_name",
+                "seller@google.com", "010-1111-1111", "seller_address", "account"));
+        memberService.joinBuyer(new MemberJoinRequest("buyer", "buyer_password", "buyer_name",
+                "buyer@google.com", "010-2222-2222", "buyer_address", null));
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 테스트")
+    @Rollback(value = false)
+    void memberJoinFailTest() {
+        // when
+        memberService.joinSeller(new MemberJoinRequest("seller", "failSeller_password", "failSeller_name",
+                "failSeller@google.com", "010-1234-1234", "failSeller_address", "fail_account"));
+        memberService.joinBuyer(new MemberJoinRequest("buyer", "failBuyer_password", "failBuyer_name",
+                "failBuyer@google.com", "010-2345-2345", "failBuyer_address", null));
+
+        // then
+        Assertions.assertThat(memberRepository.findByLoginId("seller").get().getName()).isNotEqualTo("failSeller_name");
+        Assertions.assertThat(memberRepository.findByLoginId("buyer").get().getName()).isNotEqualTo("failBuyer_name");
+    }
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void memberJoinSuccessTest() {
+        // when
+        memberService.joinSeller(new MemberJoinRequest("successSeller", "successSeller_password", "successSeller_name",
+                "successSeller@google.com", "010-1234-1234", "successSeller_address", "success_account"));
+        memberService.joinBuyer(new MemberJoinRequest("successBuyer", "successBuyer_password", "successBuyer_name",
+                "successBuyer@google.com", "010-2345-2345", "successBuyer_address", null));
+
+        // then
+        Assertions.assertThat(memberRepository.findByLoginId("successSeller").isPresent()).isTrue();
+        Assertions.assertThat(memberRepository.findByLoginId("successBuyer").isPresent()).isTrue();
+    }
+}
