966fd36781505ff521cc5d39f9bcc07781e749ba
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-05-30 16:10:23 +0900

    메일메세지기능구현 (#6)
    * 메일메세지기능구현
    
    * Dto수정, Validated추가
    
    * 테스트케이스 추가

---
+++ src/main/java/com/likelion/ecommhub/domain/MailDto.java
@@ -0,0 +1,19 @@
+package com.likelion.ecommhub.domain;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Validated
+public class MailDto {
+	private String address;
+	private String title;
+	private String message;
+	private MultipartFile file;
+}
