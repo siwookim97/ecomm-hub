3b7267819b8955f0ed6c37f59aaad2f641a4d82f
siwookim97 <tldn12gh@gmail.com>
2023-06-01 10:32:47 +0900

    feat: 사진 Entity, Repository 추가

--- src/main/java/com/likelion/ecommhub/controller/MemberController.java
+++ src/main/java/com/likelion/ecommhub/controller/MemberController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
