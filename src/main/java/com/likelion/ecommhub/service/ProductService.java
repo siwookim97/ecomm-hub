990bc73ac0ec320cd62353b3ca950ddca15a3602
HamSeungChan <head0618@naver.com>
2023-06-01 14:19:11 +0900

    feat: Product 상품 상태 추가

--- src/main/java/com/likelion/ecommhub/service/ProductService.java
+++ src/main/java/com/likelion/ecommhub/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.likelion.ecommhub.service;
 
 import com.likelion.ecommhub.domain.Product;
+import com.likelion.ecommhub.domain.ProductState;
 import com.likelion.ecommhub.dto.ProductDto;
 import com.likelion.ecommhub.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,11 +29,19 @@ public class ProductService {
     }
 
     private Product createProduct(ProductDto productDto) {
+
+        ProductState productState = ProductState.ON_SALE;
+        
+        if(productDto.getInventory() == 0){
+            productState = ProductState.SOLD_OUT;
+        }
+        
         return new Product(
                 productDto.getName(),
                 productDto.getPrice(),
                 productDto.getDetail(),
-                productDto.getInventory()
+                productDto.getInventory(),
+                productState
         );
     }
 }
