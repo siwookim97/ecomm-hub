9350850948fd36ea078d7793b6e5a95e084ea53f
HamSeungChan <head0618@naver.com>
2023-06-02 13:40:23 +0900

    feat: Notice Repository findByTitle 추가

--- src/main/java/com/likelion/ecommhub/repository/NoticeRepository.java
+++ src/main/java/com/likelion/ecommhub/repository/NoticeRepository.java
@@ -3,5 +3,8 @@ package com.likelion.ecommhub.repository;
 import com.likelion.ecommhub.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoticeRepository extends JpaRepository<Notice,Long> {
+import java.util.Optional;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Optional<Notice> findByTitle(String title);
 }
