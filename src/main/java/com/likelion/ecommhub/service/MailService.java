4b67e365db50973a08758ec7e720beb120fbb784
hjkim2040 <hjkim2661@gmail.com>
2023-06-02 13:34:39 +0900

    fix: 장바구니 view 페이지 수정

--- src/main/java/com/likelion/ecommhub/service/MailService.java
+++ src/main/java/com/likelion/ecommhub/service/MailService.java
@@ -8,10 +8,12 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.likelion.ecommhub.domain.MailDto;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MailService {
 
 	private final JavaMailSender javaMailSender;
