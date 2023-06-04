a3b82e69b6a95d571a5fbe26e35af88ab61cf8c3
HamSeungChan <head0618@naver.com>
2023-06-05 00:09:27 +0900

    feat: NotProd에 NoticeDto 데이터 추가

--- src/main/java/com/likelion/ecommhub/initData/NotProd.java
+++ src/main/java/com/likelion/ecommhub/initData/NotProd.java
@@ -1,25 +1,14 @@
 package com.likelion.ecommhub.initData;
 
 
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
-
-import com.likelion.ecommhub.EcommhubApplication;
-import com.likelion.ecommhub.domain.Cart;
-import com.likelion.ecommhub.domain.Member;
-import com.likelion.ecommhub.domain.MemberRole;
-
-import com.likelion.ecommhub.domain.Product;
-import com.likelion.ecommhub.domain.ProductState;
+import com.likelion.ecommhub.domain.*;
 import com.likelion.ecommhub.repository.CartRepository;
 import com.likelion.ecommhub.repository.MemberRepository;
+import com.likelion.ecommhub.repository.NoticeRepository;
 import com.likelion.ecommhub.repository.ProductRepository;
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 
@@ -29,10 +18,13 @@ public class NotProd {
     private final CartRepository cartRepository;
     private final ProductRepository productRepository;
 
-    public NotProd(MemberRepository memberRepository, CartRepository cartRepository, ProductRepository productRepository) {
+    private final NoticeRepository noticeRepository;
+
+    public NotProd(MemberRepository memberRepository, CartRepository cartRepository, ProductRepository productRepository, NoticeRepository noticeRepository) {
         this.memberRepository = memberRepository;
         this.cartRepository = cartRepository;
         this.productRepository = productRepository;
+        this.noticeRepository = noticeRepository;
     }
 
     @PostConstruct
@@ -40,29 +32,30 @@ public class NotProd {
         cartRepository.deleteAll();
         memberRepository.deleteAll();
         productRepository.deleteAll();
+        noticeRepository.deleteAll();
 
 
         Member member1 = new Member("loginId1", "password1", "Seller1",
-            "seller1@email.com", "phone1", "address1",
-            MemberRole.ROLE_SELLER, "account1");
+                "seller1@email.com", "phone1", "address1",
+                MemberRole.ROLE_SELLER, "account1");
 
         Member member2 = new Member("loginId2", "password2", "Seller2",
-            "seller2@email.com", "phone2", "address2",
-            MemberRole.ROLE_SELLER, "account2");
+                "seller2@email.com", "phone2", "address2",
+                MemberRole.ROLE_SELLER, "account2");
 
         memberRepository.save(member1);
         memberRepository.save(member2);
 
-        Product product1 = new Product("상품1", 100, "상품상세설명1", 10,ProductState.ON_SALE);
+        Product product1 = new Product("상품1", 100, "상품상세설명1", 10, ProductState.ON_SALE);
         productRepository.save(product1);
 
-        Product product2 = new Product("상품2", 200, "상품상세설명2", 20,ProductState.ON_SALE);
+        Product product2 = new Product("상품2", 200, "상품상세설명2", 20, ProductState.ON_SALE);
         productRepository.save(product2);
 
-        Cart cart1 = new Cart(1L, 1L, product1,"상품1", member1, 10, 100, 1000, 10);
+        Cart cart1 = new Cart(1L, 1L, product1, "상품1", member1, 10, 100, 1000, 10);
         cartRepository.save(cart1);
 
-        Cart cart2 = new Cart(2L, 2L, product2,"상품2", member2, 10, 200, 4000, 20);
+        Cart cart2 = new Cart(2L, 2L, product2, "상품2", member2, 10, 200, 4000, 20);
         cartRepository.save(cart2);
 
         member1.addCart(cart1);
@@ -70,5 +63,11 @@ public class NotProd {
 
         memberRepository.save(member1);
         memberRepository.save(member2);
+
+        Notice notice1 = new Notice(NoticeType.SYSTEM, "제목1", "내용1");
+        Notice notice2 = new Notice(NoticeType.GENERAL, "제목2", "내용2");
+
+        noticeRepository.save(notice1);
+        noticeRepository.save(notice2);
     }
 }
