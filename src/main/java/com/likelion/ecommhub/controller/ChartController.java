038a6af30332d58bcaa0725f425e60946ea4906a
Beconfident01 <hom0726@naver.com>
2023-06-05 16:45:19 +0900

    웹에서 차트확인

--- src/main/java/com/likelion/ecommhub/controller/ChartController.java
+++ src/main/java/com/likelion/ecommhub/controller/ChartController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.likelion.ecommhub.domain.MonthlySales;
 import com.likelion.ecommhub.service.ChartService;
@@ -16,7 +17,11 @@ public class ChartController {
 	ChartService chartService;
 
 	@GetMapping("/monthly-sales/{year}")
-	public List<MonthlySales> monthlySalesList(@PathVariable("year") String year){
-		return chartService.getMonthlySales(year);
+	public ModelAndView monthlySalesList(@PathVariable("year") String year) {
+		List<MonthlySales> monthlySales = chartService.getMonthlySales(year);
+
+		ModelAndView modelAndView = new ModelAndView("ChartPage");
+		modelAndView.addObject("monthlySales", monthlySales);
+		return modelAndView;
 	}
 }
