265515c8bb693f0538ebe16a23b11253c22593b5
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-06-06 22:07:16 +0900

    검색 기능 고도화  (#56)
    * feat: 페이징을 위한 대량 테스트 데이터 생성
    
    * feat: Product Repository 페이지 타입 리턴 findAll구현
    
    * chore: QueryDsl dependency 추가
    
    * feat: QueryDsl config 추가
    
    * feat: 페이징을 위한 대량 테스트 데이터 생성
    
    * feat: Product Repository 페이지 타입 리턴 findAll구현
    
    * feat: Product Service 페이지로 변환 & 페이지 크기 12로 설정
    
    * refactor: Product Repository 페이지로 변환
    
    * refactor: Product View 페이지로 변환
    
    * refactor: Product View 페이지 이동 기능 구현
    
    * feat: 상품 검색 페이징 기능 추가
    
    ---------
    
    Co-authored-by: siwookim97 <tldn12gh@gmail.com>
    Co-authored-by: Siwoo <72070679+siwookim97@users.noreply.github.com>

--- src/main/java/com/likelion/ecommhub/controller/ProductController.java
+++ src/main/java/com/likelion/ecommhub/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.likelion.ecommhub.service.ImageService;
 import com.likelion.ecommhub.service.MemberService;
 import com.likelion.ecommhub.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,10 +26,9 @@ public class ProductController {
     private final ImageService imageService;
 
     @GetMapping("/home")
-    public String showProducts(Model model) {
-        List<Product> productList = productService.findAllProducts();
-        model.addAttribute("products", productList);
-
+    public String showProducts(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Product> pagingProducts = productService.findAllProducts(page);
+        model.addAttribute("pagingProducts", pagingProducts);
         return "product/home";
     }
 
@@ -50,10 +49,11 @@ public class ProductController {
         return "redirect:/product/home";
     }
 
-    @PostMapping("/search")
-    public String searchProduct(@RequestParam("keyword") String keyword,Model model){
-        List<Product> products = productService.searchProduct(keyword);
-        model.addAttribute("products",products);
-        return "product/home";
+    @GetMapping("/search")
+    public String searchProduct(@RequestParam("keyword") String keyword, Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Product> pagingProducts = productService.searchProduct(keyword, page);
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("pagingProducts", pagingProducts);
+        return "product/search";
     }
 }
--- src/main/java/com/likelion/ecommhub/initData/NotProd.java
+++ src/main/java/com/likelion/ecommhub/initData/NotProd.java
@@ -19,6 +19,48 @@ import org.springframework.context.annotation.Configuration;
 
 public class NotProd {
 
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    public NotProd(MemberRepository memberRepository, CartRepository cartRepository,
+                   ProductRepository productRepository, BCryptPasswordEncoder encoder) {
+        this.memberRepository = memberRepository;
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+        this.encoder = encoder;
+    }
+
+    @PostConstruct
+    public void initializeData() {
+        cartRepository.deleteAll();
+        memberRepository.deleteAll();
+        productRepository.deleteAll();
+
+        Member member1 = new Member("loginId1", encoder.encode("password1"), "Seller1",
+                "seller1@email.com", "010-1234-1234", "address1",
+                MemberRole.ROLE_SELLER);
+
+        Member member2 = new Member("loginId2", encoder.encode("password2"), "Seller2",
+                "seller2@email.com", "010-1111-1111", "address2",
+                MemberRole.ROLE_SELLER);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Product product1 = new Product("상품1", 100, "상품상세설명1", 10, ProductState.ON_SALE);
+        productRepository.save(product1);
+
+        Product product2 = new Product("상품2", 200, "상품상세설명2", 20, ProductState.ON_SALE);
+        productRepository.save(product2);
+
+        for (int i = 3; i <= 300; i++) {
+            String name = String.format("상품%d",i);
+            Product product = new Product(name, 100, "상품상세설명", 10, ProductState.ON_SALE);
+            productRepository.save(product);
+        }
+    }
 //    private final MemberRepository memberRepository;
 //    private final CartRepository cartRepository;
 //    private final ProductRepository productRepository;
