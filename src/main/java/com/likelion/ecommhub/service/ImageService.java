27754eeec42cb7758a8102cad88d526408f972e1
siwookim97 <tldn12gh@gmail.com>
2023-06-10 01:34:10 +0900

    feat: AWS S3 같은 이름의 파일을 덮어저장하지 않도록

--- src/main/java/com/likelion/ecommhub/service/ImageService.java
+++ src/main/java/com/likelion/ecommhub/service/ImageService.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +31,7 @@ public class ImageService {
         List<Image> images = new ArrayList<>();
 
         for (MultipartFile multipartFile : multipartFiles) {
-            String originalFilename = multipartFile.getOriginalFilename();
+            String originalFilename = multipartFile.getOriginalFilename() + "_" + UUID.randomUUID();
 
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentLength(multipartFile.getSize());
