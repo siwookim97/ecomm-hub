5d699679a946a947b9d396f9295a620ff8ec4296
siwookim97 <tldn12gh@gmail.com>
2023-06-15 02:28:13 +0900

    refactor: 리뷰 가능한 회원인지 확인하는 로직 추가

--- src/main/java/com/likelion/ecommhub/service/ReviewService.java
+++ src/main/java/com/likelion/ecommhub/service/ReviewService.java
@@ -1,11 +1,16 @@
 package com.likelion.ecommhub.service;
 
 import com.likelion.ecommhub.domain.Member;
+import com.likelion.ecommhub.domain.Order;
 import com.likelion.ecommhub.domain.Product;
 import com.likelion.ecommhub.domain.Review;
 import com.likelion.ecommhub.dto.ReviewDto;
+import com.likelion.ecommhub.repository.MemberRepository;
+import com.likelion.ecommhub.repository.ProductRepository;
 import com.likelion.ecommhub.repository.ReviewRepository;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,29 +20,58 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
 
     @Transactional
-    public Review postReview(ReviewDto reviewDto, Member member, Product product) {
-        // TODO: 주문 테이블 검증로직 필요
+    public Review postReview(ReviewDto reviewDto, Long memberId, Long productId) {
+        Member findMember = findMember(memberId);
+        Product findProduct = findProduct(productId);
+        Order findMemberOrder = findMember.getOrders().stream()
+                .filter(order -> order.getMember().equals(findMember))
+                .findFirst()
+                .orElse(null);
 
         Review createdReview = Review.builder()
                 .title(reviewDto.getTitle())
                 .content(reviewDto.getContent())
                 .build();
-        createdReview.setMember(member);
-        createdReview.setProduct(product);
-        reviewRepository.save(createdReview);
+        createdReview.setMember(findMember);
+        createdReview.setProduct(findProduct);
+
+        if (findMemberOrder != null) {
+            reviewRepository.save(createdReview);
+        }
 
         return createdReview;
     }
 
     @Transactional
-    public void deleteReview(Long reviewId, Member member) {
-        // TODO: 주문 테이블 검증로직 필요
+    public void deleteReview(Long reviewId, Long memberId) {
+        Review findReview = findReview(reviewId);
+        Member findMember = findMember(memberId);
 
-        reviewRepository.deleteById(reviewId);
+        if (findReview.getMember().getId().equals(findMember.getId())) {
+            reviewRepository.delete(findReview);
+        }
     }
+
     public List<Review> getReviewList(Long productId) {
         return reviewRepository.findByProductId(productId);
+    }
+
+    private Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("no such MEMBER"));
+    }
+
+    private Product findProduct(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("no such PRODUCT"));
+    }
+
+    private Review findReview(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new IllegalArgumentException("no such REVIEW"));
     }
 }
