42890d42320812d3020aa3b725dc92780d655c6f
Beconfident01 <122251142+Beconfident01@users.noreply.github.com>
2023-06-15 19:51:33 +0900

    Fix 172 (#173)
    * fix: 판매자 아이디마다 다르게 통계나오도록 수정
    
    * fix: 테이블 잘리는 오류 수정
    
    * fix:yml 포트수정

--- src/main/java/com/likelion/ecommhub/controller/SalesController.java
+++ src/main/java/com/likelion/ecommhub/controller/SalesController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.likelion.ecommhub.config.auth.MemberDetails;
 import com.likelion.ecommhub.service.SalesService;
 
 @Controller
@@ -16,21 +20,15 @@ import com.likelion.ecommhub.service.SalesService;
 public class SalesController {
 
 	private final SalesService salesService;
-
+	@PreAuthorize("hasRole('ROLE_SELLER')")
 	@GetMapping("/usr/member/monthly-sales/{year}")
-	public String getMonthlySalesByYear(@PathVariable int year, Model model) {
-		List<Map<String, Object>> salesData = salesService.getMonthlySalesByYear(year);
+	public String getMonthlySalesByYear(@PathVariable int year, Model model, @AuthenticationPrincipal MemberDetails memberDetails) {
+		Long memberId = memberDetails.getMember().getId();
+
+		List<Map<String, Object>> salesData = salesService.getMonthlySalesByYearAndMemberId(year, memberId);
 		model.addAttribute("salesData", salesData);
 		model.addAttribute("year", year);
 		return "usr/member/monthly-sales";
 	}
 
-	@GetMapping("/myPage")
-	public String getMonthlySales(Model model) {
-		int year = 2023;
-		List<Map<String, Object>> salesData = salesService.getMonthlySalesByYear(year);
-		model.addAttribute("salesData", salesData);
-		model.addAttribute("year", year);
-		return "usr/member/memberPage";
-	}
 }
