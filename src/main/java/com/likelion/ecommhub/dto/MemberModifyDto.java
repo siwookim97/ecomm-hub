173f3884df83335ff4f5b6b0532bfa772a824c01
HamSeungChan <head0618@naver.com>
2023-06-15 10:18:28 +0900

    refactor: view에서 valid 처리

--- src/main/java/com/likelion/ecommhub/dto/MemberModifyDto.java
+++ src/main/java/com/likelion/ecommhub/dto/MemberModifyDto.java
@@ -5,25 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberModifyDto {
-    @NotEmpty(message = "회원 이름은 공백이 불가합니다.")
     private String nickname;
-
-    @Email(message = "회원 이메일은 공백이 불가합니다.")
     private String email;
-
-    @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$",
-            message = "올바른 전화번호 형식을 지켜주세요 ex) 01012345678")
-    private String phone; // Refactor: 정규식과 Validation 메시지 클래스로 따로 뺄 것
-
-    @NotEmpty(message = "회원 주소는 공백이 불가합니다.")
+    private String phone;
     private String address;
 }
